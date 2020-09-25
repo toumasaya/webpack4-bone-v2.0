1dbfa8061d5667b5738fa9c08632fdb009e5e967
Salt <luffy.w.pirate@gmail.com>
2020-09-25 15:50:24 +0800

    template: Add Bootstrap v5

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
 import './sass/main.sass';
-
-// ES6 syntax example
-const fn = () => 1;
-
-let a = 2;
-
-class Person {}
-
-const imgs = Array.from(document.querySelectorAll('img'));
-
-console.log('Hello');
-
-imgs.forEach((element) => {
-  element.addEventListener('load', function () {
-    console.log('load image');
-  });
-});
